3bbd05083b0942bb916a5e48a9777542d2fcef23
Clément Heme <clementhemesecond@gmail.com>
2026-05-31 20:02:25 +0200

    prevent to send essence when max is reached

--- Archipelago/Data/Items/Essence.cs
+++ Archipelago/Data/Items/Essence.cs
@@ -1,19 +1,19 @@
 ﻿namespace SplasherArchipelago.Data.Items {
     static class Essence {
-        private const uint max = 700;
+        public const uint MAX = 700;
         private static uint ammount = 0;
 
         public static void Add(uint n) => ammount += n;
         public static uint Release() {
-            if (ammount <= max) {
+            if (ammount <= MAX) {
                 var ret = ammount;
                 ammount = 0;
 
                 return ret;
             }
 
-            ammount -= max;
-            return max;
+            ammount -= MAX;
+            return MAX;
         }
     }
 }
--- Archipelago/Patches/Controller/Essence.cs
+++ Archipelago/Patches/Controller/Essence.cs
@@ -4,7 +4,7 @@ namespace SplasherArchipelago.Patches.Controller {
     [HarmonyPatch(typeof(StarManager), "OnCheckpoint")]
     public static class Essence {
         public static bool Prefix(StarManager __instance) {
-            if (GameManager.HasCollectables) {
+            if (GameManager.HasCollectables && __instance.CurrentScore < Data.Items.Essence.MAX) {
                 __instance.Add((int)Data.Items.Essence.Release());
             }
             return true;
