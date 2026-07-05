27e18d12d73b2d6bc070e16a3901e580272891d0
Clément Heme <clementhemesecond@gmail.com>
2026-07-05 18:27:34 +0200

    fix splasher collect

--- Archipelago/Patches/Location/SplasherCollect.cs
+++ Archipelago/Patches/Location/SplasherCollect.cs
@@ -5,13 +5,17 @@
  */
 
 namespace SplasherArchipelago.Patches.Location {
-    [HarmonyPatch(typeof(Splasher), "OnCheckpoint")]
+    [HarmonyPatch(typeof(GameActor), "NoReset", MethodType.Setter)]
     public static class SplasherCollect {
-        public static bool Prefix(Splasher __instance, int ___index) {
-            if (!__instance.Rescued) return true;
+        public static bool Prefix(GameActor __instance, bool value) {
+            if (!(__instance is Splasher)) return true;
 
+            var instance = (Splasher)__instance;
+            if (!value || !instance.Rescued) return true;
+
+            var index = (int)AccessTools.DeclaredField(typeof(Splasher), "index").GetValue(instance);
             var name = GameData.Instance.CurrentLevelMetaData.LevelName;
-            Data.Locations.Splashers.Check(name, ___index);
+            Data.Locations.Splashers.Check(name, index);
             return true;
         }
     }
